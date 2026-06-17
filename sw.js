19f5a2eb3d92a68aa3f8e7940f72caba65aef893
Diego <diazdiego011@gmail.com>
2026-06-17 14:09:18 -0400

    Fix mobile cache and agenda layout

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * ============================================================
  */
 
-const CACHE_NAME = 'blade-co-v1';
+const CACHE_NAME = 'blade-co-v2';
 
 /** Archivos que se cachean al instalar el SW */
 const STATIC_ASSETS = [
